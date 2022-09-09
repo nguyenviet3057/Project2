f730f8bf829caa2db193b9ef0edf6124952e8057
anhduc0125 <vuducanh0125@gmail.com>
2022-09-09 23:28:12 +0700

    Insert cho bảng status và permission

--- Database/DBmysql.sql
+++ Database/DBmysql.sql
@@ -1,138 +1,475 @@
-CREATE TABLE `permission` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `role` varchar(25)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 09, 2022 at 06:22 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
-CREATE TABLE `staff` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `fullname` varchar(60) NOT NULL,
-  `birth` date,
-  `phone` varchar(16),
-  `address` varchar(200),
-  `email` varchar(50),
-  `password` varchar(30),
-  `pic` varchar(150),
-  `salary` float,
-  `permission_id` int
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE `class` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(10) UNIQUE,
-  `n_std` int,
-  `staff_id` int
-);
 
-CREATE TABLE `subject` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(60),
-  `session` int,
-  `staff_id` int
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `semester` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `semester_no` int,
-  `subject_id` int,
-  `start_year` date
-);
+--
+-- Database: `eproject`
+--
 
-CREATE TABLE `status` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `desc` varchar(20)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE `students` (
-  `rollno` int PRIMARY KEY,
-  `fullname` varchar(60) NOT NULL,
-  `birth` date,
-  `phone` varchar(16),
-  `address` varchar(200),
-  `email` varchar(50),
-  `password` varchar(30),
-  `pic` varchar(150),
-  `status_id` int,
-  `semester_id` int,
-  `class_id` int
-);
-
-CREATE TABLE `schedule` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `staff_id` int,
-  `subject_id` int,
-  `class_id` int,
-  `start_date` date,
-  `end_date` date
-);
-
-CREATE TABLE `booking` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `schedule_id` int,
-  `begin_time` datetime,
-  `end_time` datetime,
-  `note` text
-);
+--
+-- Table structure for table `attendance`
+--
 
 CREATE TABLE `attendance` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `booking_id` int,
-  `attendance1` varchar(5),
-  `attendance2` varchar(5),
-  `note` text
-);
+  `id` int(11) NOT NULL,
+  `booking_id` int(11) DEFAULT NULL,
+  `attendance1` varchar(5) DEFAULT NULL,
+  `attendance2` varchar(5) DEFAULT NULL,
+  `note` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL,
+  `schedule_id` int(11) DEFAULT NULL,
+  `begin_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `note` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `n_std` int(11) DEFAULT NULL,
+  `staff_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mark`
+--
 
 CREATE TABLE `mark` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `subject_id` int,
-  `student_rollno` varchar(10),
-  `mark_t` float,
-  `mark_p` float,
-  `created_at` datetime
-);
+  `id` int(11) NOT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `student_rollno` varchar(10) DEFAULT NULL,
+  `mark_t` float DEFAULT NULL,
+  `mark_p` float DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
 
 CREATE TABLE `notifications` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `staff_id` int,
-  `content` text,
-  `created_at` datetime,
-  `updated_at` datetime
-);
+  `id` int(11) NOT NULL,
+  `staff_id` int(11) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permission`
+--
+
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL,
+  `role` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `permission`
+--
+
+INSERT INTO `permission` (`id`, `role`) VALUES
+(1, 'admin'),
+(2, 'system administrator'),
+(3, 'student assistant'),
+(4, 'marketing');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` int(11) NOT NULL,
+  `staff_id` int(11) DEFAULT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `semester`
+--
+
+CREATE TABLE `semester` (
+  `id` int(11) NOT NULL,
+  `semester_no` int(11) DEFAULT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `start_year` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(60) NOT NULL,
+  `birth` date DEFAULT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `pic` varchar(150) DEFAULT NULL,
+  `salary` float DEFAULT NULL,
+  `permission_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `desc` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id`, `desc`) VALUES
+(1, 'Studying'),
+(2, 'Graduated'),
+(3, 'Reserved'),
+(4, 'Dropout');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `rollno` varchar(10) NOT NULL,
+  `fullname` varchar(60) NOT NULL,
+  `birth` date DEFAULT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `pic` varchar(150) DEFAULT NULL,
+  `status_id` int(11) DEFAULT NULL,
+  `semester_id` int(11) DEFAULT NULL,
+  `class_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subclass`
+--
 
 CREATE TABLE `subclass` (
-  `student_rollno` VARCHAR(10),
-  `class_id` int
-);
+  `student_rollno` varchar(10) DEFAULT NULL,
+  `class_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `staff` ADD FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `class` ADD FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+--
+-- Table structure for table `subject`
+--
 
-ALTER TABLE `subject` ADD FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+CREATE TABLE `subject` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) DEFAULT NULL,
+  `session` int(11) DEFAULT NULL,
+  `staff_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `semester` ADD FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`);
+--
+-- Indexes for dumped tables
+--
 
-ALTER TABLE `students` ADD FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `booking_id` (`booking_id`);
 
-ALTER TABLE `students` ADD FOREIGN KEY (`semester_id`) REFERENCES `semester` (`id`);
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `schedule_id` (`schedule_id`);
 
-ALTER TABLE `students` ADD FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `staff_id` (`staff_id`);
 
-ALTER TABLE `schedule` ADD FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+--
+-- Indexes for table `mark`
+--
+ALTER TABLE `mark`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `student_rollno` (`student_rollno`);
 
-ALTER TABLE `schedule` ADD FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`);
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`staff_id`);
 
-ALTER TABLE `schedule` ADD FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
+--
+-- Indexes for table `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `booking` ADD FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`);
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`staff_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `class_id` (`class_id`);
 
-ALTER TABLE `attendance` ADD FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`);
+--
+-- Indexes for table `semester`
+--
+ALTER TABLE `semester`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subject_id` (`subject_id`);
 
-ALTER TABLE `mark` ADD FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`);
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `permission_id` (`permission_id`);
 
-ALTER TABLE `mark` ADD FOREIGN KEY (`student_rollno`) REFERENCES `students` (`rollno`);
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `notifications` ADD FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`rollno`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `semester_id` (`semester_id`),
+  ADD KEY `status_id` (`status_id`);
 
-ALTER TABLE `subclass` ADD FOREIGN KEY (`student_rollno`) REFERENCES `students` (`rollno`);
+--
+-- Indexes for table `subclass`
+--
+ALTER TABLE `subclass`
+  ADD KEY `student_rollno` (`student_rollno`),
+  ADD KEY `class_id` (`class_id`);
 
-ALTER TABLE `subclass` ADD FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mark`
+--
+ALTER TABLE `mark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `semester`
+--
+ALTER TABLE `semester`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`id`);
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`);
+
+--
+-- Constraints for table `class`
+--
+ALTER TABLE `class`
+  ADD CONSTRAINT `class_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+
+--
+-- Constraints for table `mark`
+--
+ALTER TABLE `mark`
+  ADD CONSTRAINT `mark_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`),
+  ADD CONSTRAINT `mark_ibfk_2` FOREIGN KEY (`student_rollno`) REFERENCES `students` (`rollno`);
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+
+--
+-- Constraints for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`),
+  ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`),
+  ADD CONSTRAINT `schedule_ibfk_3` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
+
+--
+-- Constraints for table `semester`
+--
+ALTER TABLE `semester`
+  ADD CONSTRAINT `semester_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`semester_id`) REFERENCES `semester` (`id`),
+  ADD CONSTRAINT `students_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
+
+--
+-- Constraints for table `subclass`
+--
+ALTER TABLE `subclass`
+  ADD CONSTRAINT `subclass_ibfk_1` FOREIGN KEY (`student_rollno`) REFERENCES `students` (`rollno`),
+  ADD CONSTRAINT `subclass_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`);
+
+--
+-- Constraints for table `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `subject_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
