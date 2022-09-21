6530731b32944c2a9d6e189947ea274ba426bce7
anhduc0125 <vuducanh0125@gmail.com>
2022-09-21 18:51:50 +0700

    Fix lại hai bên sidebar

--- Layout/VuDucAnh/EProject2/src/main/java/com/group2/eproject2/App.java
+++ Layout/VuDucAnh/EProject2/src/main/java/com/group2/eproject2/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("class"), 1280, 720);
+        scene = new Scene(loadFXML("mark_1"), 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
